5a111d04768b5d14bd7de5acacd8e714a93a4355
sanketdevakar <sanketdevakar09@gmail.com>
2025-05-14 21:26:06 +0530

    Data ingestion

--- src/exception.py
+++ src/exception.py
@@ -1,8 +1,9 @@
 import sys
-from src.logger import logging
+import logger
+from logger import logging
 
 def error_message_detail(error,error_detail:sys):
-    _,_,exc_tb=exc_tb.exc_info()
+    _,_,exc_tb=error_detail.exc_info()
     file_name = exc_tb.tb_frame.f_code.co_filename
     error_message = "Error occured in python script name [{0}] line number[{1}] error message [{2}]".format(file_name,exc_tb.tb_lineno,str(error))
 
@@ -15,3 +16,4 @@ class CustomException(Exception):
 
     def __str__(self):
         return self.error_message
+    
