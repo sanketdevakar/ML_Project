5277dd18003a8f0fd1128a6a611f0fc96cf8c67d
sanketdevakar <sanketdevakar09@gmail.com>
2025-05-16 13:07:58 +0530

    Added code for Model Trainer

--- src/utils.py
+++ src/utils.py
@@ -6,6 +6,7 @@ import pandas as pd
 import dill
 import pickle
 from src.exception import CustomException
+from sklearn.metrics import r2_score
 
 def save_object(file_path, obj):
     try:
@@ -18,3 +19,29 @@ def save_object(file_path, obj):
 
     except Exception as e:
         raise CustomException(e, sys)
+    
+def evaluate_model(X_train, y_train,X_test,y_test,models):
+    try:
+        report = {}
+
+        for i in range(len(list(models))):
+            model = list(models.values())[i]
+
+            model.fit(X_train, y_train)  # Train model
+
+            y_train_predict = model.predict(X_train)
+            y_test_predict = model.predict(X_test)
+
+            train_model_score = r2_score(y_train,y_train_predict)
+
+            test_model_score = r2_score(y_test,y_test_predict)
+
+            report[list(models.keys())[i]] = test_model_score
+
+        
+        return report
+        
+
+    except Exception as e:
+
+        raise CustomException(e,sys)
